7af0f1cbe7afa19cf2b4ca3b8c4717828313c08b
srijandubey23 <srijandubey639@gmail.com>
2021-04-08 17:06:31 +0530

    mouse drag and release

--- SlingShot.js
+++ SlingShot.js
@@ -12,11 +12,16 @@ class SlingShot{
     }
 
       display(){
+          if(this.SlingShot.bodyA){
             var pointA = this.SlingShot.bodyA.position;
             var pointB = this.pointB;
             strokeWeight(4);
             line(pointA.x, pointA.y, pointB.x, pointB.y);
-        
+          }
+    }
+    fly(){
+
+        this.SlingShot.bodyA=null;
     }
     
 }
